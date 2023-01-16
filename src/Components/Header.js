0d085a3af054647a10290733f50a40a62bb5f8f4
David Lasky <lasky96@gmail.com>
2023-01-16 10:13:09 -0500

    key-frames

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,14 @@ export default function Header() {
         <div className='header-container'>
             <h1>Hello, my name is David Lasky</h1>
             <h3>I'm a developer who is always trying to learn and remain a student for life</h3>
-            <h4>My hobbies: Guitar, hiking, trying new codes</h4>
+            <h4>My hobbies: Guitar, hiking, trying new code</h4>
+            <section className="skill-tree">
+                <h2>My Skills:</h2>
+                <img></img>
+                <img></img>
+                <img></img>
+                <img></img>
+            </section>
         </div> 
     )
 }
