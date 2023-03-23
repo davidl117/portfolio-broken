9ec45e165c92f1e5d20af6b3e8ebc5f2b1afd032
David Lasky <lasky96@gmail.com>
2023-03-23 14:14:06 -0400

    added screenshot of projects

--- src/Components/data.js
+++ src/Components/data.js
@@ -5,19 +5,20 @@ export default  [
             descriptionLength: 80,
             link: "https://github.com/davidl117/todo-list",
             demo: "https://davidl117.github.io/Todo/todo.html",
-            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fonplanners.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Ftemplate_fancy%2Fpublic%2Ftemplate-images%2Fprintable-daily-do-list-template_0.png&f=1&nofb=1&ipt=bac47b25a2af55e22cc0ede61eac7f72bf9449f59a88116c65263cc292d781b3&ipo=images",
+            image: "./images/Users/lasky/Desktop/Pictures/Screenshots/todoscreenshot.jpg",
         },
         {
-            description: "",
+            description: "This is a bakery website built in React and with multiple pages. It is a build for a client and is a work in progress.",
             descriptionLength: 80,
             link: "https://github.com/davidl117/The-Bakery-Uncommon",
-            image: "bakery.png", 
+            demo:"",
+            image: "./images/Users/lasky/Desktop/Pictures/Screenshots/bakeryscreenshot.jpg", 
         },
         {
             description: "This is my website where my work is located and displayed as my portfolio. This site is going to be updated as my skills improve and the projects will be updated as well. ",
             descriptionLength: 80,
             link:"https://github.com/davidl117/portfolio",
-            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsvitla.com%2Fuploads%2F0%2F1704-websiteg.jpg%3F1552573200&f=1&nofb=1&ipt=6a1ed359db56c43f7318a78588e6c222a60d62d22b7d1bbfaf0b835793b81267&ipo=images",
+            image: "./images/Users/lasky/Desktop/Pictures/Screenshots/portfolioscreenshot.jpg",
             
         }
     ]
